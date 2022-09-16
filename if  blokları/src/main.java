2a52161e02c75f8f515d5771734f84d71991620e
BurakDerse <113616771+BurakDerse@users.noreply.github.com>
2022-09-16 05:50:10 +0300

    Add files via upload

---
+++ if  blokları/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi = 2;
+		if(sayi<3) {
+			System.out.println("Sayı 3 den küçük");
+	}
+		else if(sayi>3) {
+			System.out.println("Sayı 3 den büyük");		
+	}
+		else{
+			System.out.println("Sayı 3 e eşit");
+		}
+	}
+
+}
