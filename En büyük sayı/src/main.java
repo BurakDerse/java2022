d5f1f45d1aaf04993bdaab36bd9a0d2a2e50494d
BurakDerse <113616771+BurakDerse@users.noreply.github.com>
2022-09-16 05:51:11 +0300

    Add files via upload

---
+++ En büyük sayı/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi1=2;
+		int sayi2=3;
+		int sayi3=4;
+		int sayib=sayi1;
+		
+		if(sayi2>sayib) {
+			sayib=sayi2;
+		}
+		if(sayi3>sayib) {
+			sayib=sayi3;
+		}
+		System.out.println(sayib);
+	}
+
+}
