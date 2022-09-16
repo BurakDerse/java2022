842a8a0f941514670baf303e9d0e15312e1301dc
BurakDerse <113616771+BurakDerse@users.noreply.github.com>
2022-09-16 05:48:43 +0300

    Add files via upload

---
+++ değişkenler/src/değişkenler/main.java
@@ -0,0 +1,20 @@
+package değişkenler;
+
+public class main {
+
+	public static void main(String[] args) {
+		int ogrenciNumarası = 2 ;
+		String yazi ="Öğrenci numarası :";
+		
+		System.out.println(yazi + ogrenciNumarası);
+		System.out.println(yazi + ogrenciNumarası);
+		
+		boolean dogruMu = false;
+		double sayi = 10.2;
+		
+		System.out.println(dogruMu);
+		System.out.println(sayi);
+
+	}
+
+}
