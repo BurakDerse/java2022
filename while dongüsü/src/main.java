c1c9b5d0ac659001dc21fc363311f252c2d1e8be
BurakDerse <113616771+BurakDerse@users.noreply.github.com>
2022-09-16 05:51:48 +0300

    Add files via upload

---
+++ while dongüsü/src/main.java
@@ -0,0 +1,20 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("bitti");
+		
+		int j =0;
+		do {
+			System.out.println(j);
+			j++;
+		}while(j<10);
+		System.out.println("bitti");
+
+	}	
+}
